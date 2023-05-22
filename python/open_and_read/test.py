277a01ef8e4b809e154ed0f81ec6be214a452ea9
Jung Woo Lee <phyjics@gmail.com>
2023-05-22 22:14:56 +0900

    file

---
+++ python/open_and_read/test.py
@@ -0,0 +1,12 @@
+file = open("file")
+print("=============================== readlines")
+print(file.readlines())
+
+file = open("file")
+print("=============================== readline x1 + read")
+print(file.readline())
+print(file.read())
+
+file = open("file")
+print("=============================== read")
+print(file.read())
