ae43e59e2d8a3f234e26bdc13c9c01adee7fbbd5
ejungwoo <phyjics@gmail.com>
2023-10-29 15:23:36 +0900

    ttreeselection this is much more stable version

--- root/ttreeselection/test.C
+++ root/ttreeselection/test.C
@@ -1,33 +1,59 @@
 void test()
 {
     const char *selection = "EventHeader[0].fIsGoodEvent";
+    //const char *selection = "Entry$==2";
     //const char *selection = "1";
 
     auto file = new TFile("~/data/texat/reco/texat_0801.all.root");
-    auto tree = (TTree*) file -> Get("event");
+    auto fInputTree = (TTree*) file -> Get("event");
+    TClonesArray* fEventHeaderHolder = nullptr;
+    fInputTree -> SetBranchAddress("EventHeader",&fEventHeaderHolder);
 
-    tree -> Draw(">>ha",selection,"entrylist");
-    TEventList *elist = (TEventList*)gDirectory->Get("ha");
-    elist -> ls();
-    return;
+    fInputTree -> Draw(">>lkentrylist",selection,"entrylist");
+    TEntryList *entryList = (TEntryList*) gDirectory -> Get("lkentrylist");
+    Long64_t numSelEntries = entryList -> GetN();
+    Long64_t chainEntries = fInputTree -> GetEntries();
+    Int_t fTreeNumber = -1;
+    fInputTree -> SetEntryList(entryList);
 
-    auto fSelect = new TTreeFormula("LKTTreeSelection",selection,tree);
+    //for (auto iSelEntry=0; iSelEntry<numSelEntries; ++iSelEntry)
+    for (auto iSelEntry=0; iSelEntry<100; ++iSelEntry)
+    {
+        auto entryNumber = fInputTree -> GetEntryNumber(iSelEntry);
+        if (entryNumber<0) {
+            cout << "entryNumber<0" << endl;
+            break;
+        }
+        auto localEntry = fInputTree -> LoadTree(entryNumber);
+        if (localEntry<0) {
+            cout << "localEntry<0" << endl;
+            break;
+        }
+        fInputTree -> GetEntry(entryNumber);
+        cout << iSelEntry << " " << entryNumber << " " << localEntry << endl;
+        auto eventHeader = (TTEventHeader*) fEventHeaderHolder -> At(0);
+        cout << eventHeader << endl;
+        eventHeader -> Print();
+    }
 
-    Int_t tnumber = -1;
-    auto numEntries = tree -> GetEntries();
+    /*
+    auto fSelect = new TTreeFormula("LKTTreeSelection",selection,fInputTree);
+
+    Int_t fTreeNumber = -1;
+    auto numEntries = fInputTree -> GetEntries();
     //for (auto entry=0; entry<numEntries; ++entry)
     //for (auto entry=0; entry<10; ++entry)
     for (auto entry=0; entry<10; ++entry)
     {
-        auto entryNumber = tree->GetEntryNumber(entry);
+        auto entryNumber = fInputTree > GetEntryNumber(entry);
         //lk_debug << entry << " " << entryNumber << endl;
         if (entryNumber < 0) break;
-        Long64_t localEntry = tree -> LoadTree(entryNumber);
+        Long64_t localEntry = fInputTree -> LoadTree(entryNumber);
 
         if (localEntry < 0) break;
-        if (tnumber != tree->GetTreeNumber()) {
-            tnumber = tree->GetTreeNumber();
-            if (fSelect) fSelect->UpdateFormulaLeaves();
+        if (fTreeNumber != fInputTree->GetTreeNumber()) {
+            fTreeNumber = fInputTree -> GetTreeNumber();
+            if (fSelect) fSelect -> UpdateFormulaLeaves();
         }
 
         if (fSelect){
@@ -41,7 +67,5 @@ void test()
             //lk_debug << ">> " << entry << " " << keep << endl;
         }
     }
-
-
-    //cout << ndata << endl;
+    */
 }
